9685bd89d7b84c3645a90ff7e19d756cf3714916
Prabodh Raj U R <prabodhrajmow@gmail.com>
2025-09-21 00:42:35 +0530

    chore: initial codegen

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# autoapp-brahma-shop
+# Project
+
+Generated for: framework: next
+Build me an ecommerce app with a products page...
---
+++ server.js
@@ -0,0 +1,2 @@
+const http=require('http');http.createServer((_,res)=>res.end("Hello from framework: next
+Build me an ecommerce app with a products page...!")).listen(3000);
